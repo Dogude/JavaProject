eff2f5f7e1efb817789275f1bbb20a1647a16b7e
North <131747396+Dogude@users.noreply.github.com>
2026-05-08 15:55:55 +0200

    Create  FirstRestApiApplication.java

---
+++ FirstRestApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.firstrestapi;
+
+// Task 2
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestApiApplication.class, args);
+	}
+
+}
